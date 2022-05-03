75ef9bbad14da2d6695d8582a18e938b5dc9e309
Ramon Nario <rtnario@yahoo.com.ph>
2022-05-03 15:04:43 +0800

    Finish basic functionality + reset and clear

--- script.js
+++ script.js
@@ -1,45 +1,71 @@
 const centerPane = document.querySelector('#center-pane');
-// centerPane.style.gridTemplateColumns = "50px 50px 50px";
 
 let gridSize = 0;
 let validNum = false;
 
-while (!validNum) {
-    gridSize = parseInt(prompt("Input the grid size (1-100)"));
-    
-    if (gridSize === 0 || gridSize < 0 || gridSize > 100) {
-        alert("Grid size is out of range!");
+function generateGrid() {
+    while (!validNum) {
+        gridSize = parseInt(prompt("Input the number of squares per side (1-100)"));
+        
+        if (gridSize < 1 || gridSize > 100) {
+            alert("Value is out of range!");
+        }
+        else {
+            validNum = true;
+        }
     }
 
-    else {
-        validNum = true;
+    clearGrid();
+
+    let gridTemplateColumns = "";
+    let gridTemplateRows = "";
+
+    for (let i = 0; i < gridSize; i++) {
+        gridTemplateColumns += "1fr ";
+        gridTemplateRows += "1fr ";
     }
+
+    let numOfSquares = gridSize * gridSize;
+
+    for (let i = 0; i < numOfSquares; i++) {
+        const square = document.createElement('div');
+        square.classList = "canvas-square";
+        centerPane.append(square);
+    }
+
+    centerPane.style.gridTemplateColumns = gridTemplateColumns;
+    centerPane.style.gridTemplateRows = gridTemplateRows;
+
+    const setBg = () => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        return "#" + randomColor;
+    }
+
+    const squares = document.querySelectorAll('.canvas-square');
+
+    squares.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        })
+    });
 }
 
-let gridTemplateColumns = "";
-let gridTemplateRows = "";
-
-
-for (let i = 0; i < gridSize; i++) {
-    gridTemplateColumns += "1fr ";
-    gridTemplateRows += "1fr ";
+function clearGrid() {
+    const squares = document.querySelectorAll('.canvas-square');
+    squares.forEach(square => square.style.backgroundColor = "white");
 }
 
-let numOfSquares = gridSize * gridSize;
+const btnReset = document.querySelector('#btn-reset');
 
-for (let i = 0; i < numOfSquares; i++) {
-    const square = document.createElement('div');
-    square.classList = "canvas-square";
-    centerPane.append(square);
-}
+btnReset.addEventListener('click', () => {
+    validNum = false;
+    generateGrid();
+});
 
-centerPane.style.gridTemplateColumns = gridTemplateColumns;
-centerPane.style.gridTemplateRows = gridTemplateRows;
+const btnClear = document.querySelector('#btn-clear');
 
-const setBg = () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
-    return "#" + randomColor;
-}
+btnClear.addEventListener('click', () => {
+    clearGrid();
+});
 
-const squares = document.querySelectorAll('.canvas-square');
-squares.forEach(square => square.style.backgroundColor = setBg());
+generateGrid();
